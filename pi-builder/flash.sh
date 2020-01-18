6b639b12d88972ccd96b5029281d026de5ea5b3d
Andrew Suderman <andrew@sudermanjr.com>
2020-01-18 12:34:53 -0700

    fixing flash process with sops

--- pi-builder/flash.sh
+++ pi-builder/flash.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+set -e
+
+sops -d cloud-init.yaml > init.yaml
 flash \
     --bootconf no-uart-config.txt \
-    --userdata cloud-init.yaml \
+    --userdata init.yaml \
     https://github.com/hypriot/image-builder-rpi/releases/download/v1.12.0/hypriotos-rpi-v1.12.0.img.zip
+
+rm init.yaml
